f0d08a20c13428d93ce479f8832f820c0d71b908
KUrushi <boogie_of_lesfreres@me.com>
2021-04-19 11:14:31 +0900

    clap crateから構造体を利用するように変更

--- ch04/samplecli/src/main.rs
+++ ch04/samplecli/src/main.rs
@@ -1,31 +1,30 @@
 use clap::{App, Arg};
 
-fn main() {
-    let matches = App::new("My RPN program")
-        .version("1.0.0")
-        .author("Your name")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::new("formula_file")
-                .about("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::new("verbose")
-                .about("Sets the level of verbosity")
-                .short('v')
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
+#[derive(Clap, Debug)]
+#[clap(
+    name="My PRN program",
+    version="1.0.0",
+    name= "KUrushi",
+    about = "Super awesome sample PRN calculator"
+)]
 
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified: {}", file),
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+    /// Formulas written in PRN
+    #[clap(name="FILE")]
+    formula_file = Option<String>,
+}
+
+fn main() {
+    let opts = Opts::parse();
+    match opts.formula_file {
+        Some(file) => println!("File specified:{}", file);
         None => println!("No file specified."),
     }
 
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    println!("Is verbosity specified?: {}", opts.verbose);
+:wq
+
 }
