1370a6718780f5db6d3ccf41c3a316b1a7b63415
KUrushi <boogie_of_lesfreres@me.com>
2021-04-19 15:51:04 +0900

    計算ロジックの雛形の作成

--- ch04/samplecli/src/main.rs
+++ ch04/samplecli/src/main.rs
@@ -1,10 +1,13 @@
-use clap::{App, Arg};
+use clap::Clap;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 
 #[derive(Clap, Debug)]
 #[clap(
-    name="My PRN program",
-    version="1.0.0",
-    name= "KUrushi",
+    name = "My PRN program",
+    version = "1.0.0",
+    name = "KUrushi",
     about = "Super awesome sample PRN calculator"
 )]
 
@@ -12,19 +15,47 @@ struct Opts {
     /// Sets the level of verbosity
     #[clap(short, long)]
     verbose: bool,
+
     /// Formulas written in PRN
-    #[clap(name="FILE")]
-    formula_file = Option<String>,
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+// 逆ポーランド記法計算ロジック
+// 構造体雛形の作成
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        0
+    }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
 
 fn main() {
+    /// 1. コマンドラインから入力ファイルを指定または何も指定せず、起動する
+    /// 2. 入力を読み込む準備をする。入力ファイルが指定された時は該当ファイルから、何も指定されなかった時は標準入力から読み込めるようhにする
+    /// 3. 2で準備したものから１行ずつ読み込んでRPN計算処理関数に渡す
+    /// 4. 計算処理関数で処理した結果を標準出力に書き出す
     let opts = Opts::parse();
-    match opts.formula_file {
-        Some(file) => println!("File specified:{}", file);
-        None => println!("No file specified."),
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose);
+    } else {
+        println!("No file is specified");
     }
-
-    println!("Is verbosity specified?: {}", opts.verbose);
-:wq
-
 }
