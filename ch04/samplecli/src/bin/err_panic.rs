8c713d0e4edb199476ba07866fe49055c8ea4d31
KUrushi <boogie_of_lesfreres@me.com>
2021-04-20 10:59:13 +0900

    自作のError型を定義して、panic処理を追加

--- ch04/samplecli/src/bin/err_panic.rs
+++ ch04/samplecli/src/bin/err_panic.rs
@@ -1,14 +1,33 @@
-fn get_int_from_file() -> i32 {
+enum MyError {
+    Io(std::io:Error),
+    Num(std::num::ParseIntError),
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MyError::Io(cause) => write!(f, "I/O Error: {}", cause),
+            MyError::Num(cause) => write!(f, "Parse Error: {}", cause),
+        }
+    }
+}
+
+fn get_int_from_file() -> Result<i32, String> {
     let path = "number.txt";
-    let num_str = std::fs::read_to_string(path).expect("failed to open the file");
-    let ret = num_str
+    let num_str = std::fs::read_to_string(path).map_error(|e| MyError::Io(e))?;
+    num_str
         .trim()
         .parse::<i32>()
-        .expect("failed to parse string to a number.");
-
-    ret * 2
+        .map(|t| t * 2)
+        .map_error(|e| MyError(e))
 }
 
 fn main() {
-    println!("{}", get_int_from_file());
+    match get_int_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => match e {
+            MyError::Io(cause) => println!("I/O Error: {}", cause),
+            MyError::Num(cause) => println!("Parse Error: {}", cause),
+        }
+    }
 }
