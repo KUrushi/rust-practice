38784029741ea7ebd19c83333a6f5cf72cd8220d
KUrushi <boogie_of_lesfreres@me.com>
2021-04-20 09:47:55 +0900

    askamaのテンプレートを追加して、Errorハンドリングを追加

--- ch05/todo/src/main.rs
+++ ch05/todo/src/main.rs
@@ -1,13 +1,41 @@
-use actix_web::{get, App, HttpResponse, HttpServer}
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
+use thiserror::Error;
+
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
+
+impl ResponseError for MyError {}
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello World!";
-
-    // HttpResponse::Ok()はステータスコード200を持つHttpResponseBuilderという構造体を返す
-    // HttpResponseBuidler のbody()という関数にレスポンスのボディを渡すと HttpResponseが返ってくる
-    // 戻り値の型がResultなのでOkで包む
-    Ok(HttpResponse::Ok().body(response_body))
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "SEcond entry".to_string(),
+    });
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
